47b808edce4363bb52e62354ba6e22a0046a2a58
vmqxdv <vquintilio@gmail.com>
2025-03-03 13:43:36 +0100

    feat: Dichiarata funzione tradizionale per stampare un saluto diverso in base all'orario e stampato il risultato

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,15 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function greetingBasedOnHour(str) {
+  const now = new Date();
+  const hour = now.getHours();
 
+  return hour <= 13 ? `Buongiorno ${str}` : hour <= 17 ? `Buon pomeriggio ${str}` : `Buonasera ${str}` 
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greetingBasedOnHour(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
